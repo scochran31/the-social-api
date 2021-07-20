d8769628175dc94c0e326564fc8cd1483b83f299
Shawn Cochran <shawn.cochran31@gmail.com>
2021-07-20 15:04:39 -0600

    server.js created

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware for Project
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGOOSE_URI || 'mongodb://localhost/the-social-api', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
