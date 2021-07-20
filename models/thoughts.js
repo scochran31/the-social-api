3db8a24c0545742bf880a25d072b915e3cbce201
Shawn Cochran <shawn.cochran31@gmail.com>
2021-07-20 16:01:36 -0600

    thougth schema created

--- models/thoughts.js
+++ models/thoughts.js
@@ -5,14 +5,14 @@ const reactionSchema = new Schema(
     {
         reactionId: [
             {
-                type: Schema.Types.ObjectId,
+                type: Schema.Types.ObjectId(),
                 default: () => new Types.ObjectId()
             }
         ],
         reactionBody: {
             type: String,
             required: "Let's hear that reaction!",
-            maxLength = 240
+            maxLength: 240
         },
         username: {
             type: String,
@@ -23,4 +23,40 @@ const reactionSchema = new Schema(
             default: Date.new,
             get: dateCreated => dateFormat(dateCreatedVal)
         }
-    });
+    }
+);
+
+const thoughtSchema = new Schema(
+    {
+        thoughtBubble: {
+            type: String,
+            required: 'Words in thought bubble please!',
+            minLength: 1,
+            maxLength: 120
+        },
+        dateCreated: {
+            type: Date,
+            default: Date.new,
+            get: dateCreated => dateFormat(dateCreatedVal)
+        },
+        username: {
+            type: Schema.Types.ObjectId(),
+            required: true,
+            ref: 'User'
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            getters: true,
+        }
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
